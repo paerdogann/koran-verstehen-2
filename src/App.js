84ac1e2651d54406506d5a29c1d360784cd5ac29
elif-gmsl <elif@caredsk.de>
2026-08-26 12:19:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import TopicPage from './pages/TopicPage';
 import Profile from './pages/Profile';
-
+import Admin from './pages/Admin';
+import BeitragPage from './pages/BeitragPage';
+import CategoryPage from './pages/CategoryPage';
 export default function App() {
   return (
     <AuthProvider>
@@ -17,10 +19,13 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/themen/:slug" element={<TopicPage />} />
+          <Route path="/themen/:categoryId" element={<CategoryPage />} />
+          <Route path="/beitrag/:id" element={<BeitragPage />} />
           <Route path="/profil" element={<Profile />} />
+          <Route path="/admin" element={<Admin />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </AuthProvider>
   );
